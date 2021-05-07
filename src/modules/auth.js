4f1336f61b1c1245e988066f2ca17707f4bb42d4
Dawid <baluszynskidawid@gmail.com>
2021-05-07 20:03:46 +0200

    fix: add cors mode to fetch, created images folder

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -2,7 +2,9 @@ const ENDPOINT = (city, units) =>
   `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=3cdbc3f91e152ae949830280ac768421&units=${units}`;
 
 const fetchData = async (city, units) => {
-  const response = await fetch(ENDPOINT(city, units));
+  const response = await fetch(ENDPOINT(city, units), {
+    mode: 'cors',
+  });
   const data = await response.json();
   return data;
 };
